ee81ac255f5bcaa51dae288c92a755b40afd2b0f
zheng <yootaoo@gmail.com>
2019-04-25 20:47:10 +0200

    add a func to detransform an img of a pytorch dataset

--- visual_utils.py
+++ visual_utils.py
@@ -1,7 +1,21 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.metrics import confusion_matrix
+import torch
+import torch.nn as nn
 
+
+def detransform(img, mean: list, std: list):
+    '''Detransform an img of a pytorch dataset.
+    '''
+    mean = torch.tensor(mean)
+    std = torch.tensor(std)
+    # get the image back in [0,1] range (reverse Normalize(mean, std) process)
+    denorm_img = img * std[...,None,None] + mean[...,None,None]
+    # CxHxW -> HxWxC (reverse ToTensor() process)
+    hwc_img = np.transpose(denorm_img, (1, 2, 0))
+    # Tensor -> numpy.ndarray
+    return hwc_img.numpy()
 
 def plot_confusion_matrix(y_true, y_pred, classes, cmap=plt.cm.Blues):
     '''
@@ -34,3 +48,6 @@ def plot_confusion_matrix(y_true, y_pred, classes, cmap=plt.cm.Blues):
                     color="white" if cm[i, j] > thresh else "black")
     fig.tight_layout()
     return ax
+
+def _hook(layer: nn.Module):
+    pass
