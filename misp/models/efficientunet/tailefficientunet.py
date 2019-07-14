55305c13d37648a83ab6521a2fdf0b254af7a98e
zhoudaxia233 <yootaoo@gmail.com>
2019-07-14 23:22:48 +0200

    bug fixed: now tailefficientunet can be trained on GPU

--- misp/models/efficientunet/tailefficientunet.py
+++ misp/models/efficientunet/tailefficientunet.py
@@ -23,7 +23,7 @@ def get_blocks_to_be_concat(model, x):
                     shape = output.size()[-2:]
                     if shape not in shapes:
                         shapes.add(shape)
-                        blocks[module.name] = (output.size()[1], output)
+                        blocks[module.name] = output
 
                 elif module.name == 'head_swish':
                     # when module.name == 'head_swish', it means the program has already got all necessary blocks for
@@ -33,7 +33,7 @@ def get_blocks_to_be_concat(model, x):
                     # image size. The last block has the same image size as 'head_swish' before upscaling. So we don't
                     # really need the last block for concatenation. That's why I wrote `blocks.popitem()`.
                     blocks.popitem()
-                    blocks[module.name] = (output.size()[1], output)
+                    blocks[module.name] = output
 
             except AttributeError:
                 pass
@@ -63,45 +63,77 @@ class EfficientUnet(nn.Module):
         super().__init__()
 
         self.encoder = encoder
-        self.out_channels = out_channels
         self.concat_input = concat_input
+
+        self.up_conv1 = up_conv(self.n_channels, 512)
+        self.double_conv1 = double_conv(self.size[0], 512)
+        self.up_conv2 = up_conv(512, 256)
+        self.double_conv2 = double_conv(self.size[1], 256)
+        self.up_conv3 = up_conv(256, 128)
+        self.double_conv3 = double_conv(self.size[2], 128)
+        self.up_conv4 = up_conv(128, 64)
+        self.double_conv4 = double_conv(self.size[3], 64)
+
+        self.custom_head_in_channels = 64 * 2
+
+        if self.concat_input:
+            self.custom_head_in_channels = 32 * 2
+            self.up_conv_input = up_conv(64, 32)
+            self.double_conv_input = double_conv(self.size[4], 32)
+
+        self.maxpooling = nn.AdaptiveMaxPool2d(output_size=(1, 1))
+        self.avgpooling = nn.AdaptiveAvgPool2d(output_size=(1, 1))
+        self.custom_head = custom_head(self.custom_head_in_channels, out_channels)
+
+    @property
+    def n_channels(self):
+        n_channels_dict = {'efficientnet-b0': 1280, 'efficientnet-b1': 1280, 'efficientnet-b2': 1408,
+                           'efficientnet-b3': 1536, 'efficientnet-b4': 1792, 'efficientnet-b5': 2048,
+                           'efficientnet-b6': 2304, 'efficientnet-b7': 2560}
+        return n_channels_dict[self.encoder.name]
+
+    @property
+    def size(self):
+        size_dict = {'efficientnet-b0': [592, 296, 152, 80, 35, 32], 'efficientnet-b1': [592, 296, 152, 80, 35, 32],
+                     'efficientnet-b2': [600, 304, 152, 80, 35, 32], 'efficientnet-b3': [608, 304, 160, 88, 35, 32],
+                     'efficientnet-b4': [624, 312, 160, 88, 35, 32], 'efficientnet-b5': [640, 320, 168, 88, 35, 32],
+                     'efficientnet-b6': [656, 328, 168, 96, 35, 32], 'efficientnet-b7': [672, 336, 176, 96, 35, 32]}
+        return size_dict[self.encoder.name]
 
     def forward(self, x):
         input_ = x
 
         blocks = get_blocks_to_be_concat(self.encoder, x)
-        _, (n_channels, x) = blocks.popitem()
+        _, x = blocks.popitem()
 
-        x = up_conv(n_channels, 512)(x)
-        x = torch.cat([x, blocks.popitem()[1][1]], dim=1)
-        x = double_conv(x.size(1), 512)(x)
+        x = self.up_conv1(x)
+        x = torch.cat([x, blocks.popitem()[1]], dim=1)
+        x = self.double_conv1(x)
 
-        x = up_conv(512, 256)(x)
-        x = torch.cat([x, blocks.popitem()[1][1]], dim=1)
-        x = double_conv(x.size(1), 256)(x)
+        x = self.up_conv2(x)
+        x = torch.cat([x, blocks.popitem()[1]], dim=1)
+        x = self.double_conv2(x)
 
-        x = up_conv(256, 128)(x)
-        x = torch.cat([x, blocks.popitem()[1][1]], dim=1)
-        x = double_conv(x.size(1), 128)(x)
+        x = self.up_conv3(x)
+        x = torch.cat([x, blocks.popitem()[1]], dim=1)
+        x = self.double_conv3(x)
 
-        x = up_conv(128, 64)(x)
-        x = torch.cat([x, blocks.popitem()[1][1]], dim=1)
-        x = double_conv(x.size(1), 64)(x)
+        x = self.up_conv4(x)
+        x = torch.cat([x, blocks.popitem()[1]], dim=1)
+        x = self.double_conv4(x)
 
         if self.concat_input:
-            x = up_conv(64, 32)(x)
+            x = self.up_conv_input(x)
             x = torch.cat([x, input_], dim=1)
-            x = double_conv(x.size(1), 32)(x)
+            x = self.double_conv_input(x)
 
-        custom_head_in_channels = x.size(1) * 2
-
-        mp = nn.AdaptiveMaxPool2d(output_size=(1, 1))(x)
-        ap = nn.AdaptiveAvgPool2d(output_size=(1, 1))(x)
+        mp = self.maxpooling(x)
+        ap = self.avgpooling(x)
         x = torch.cat([mp, ap], dim=1)
 
         x = x.view(x.size(0), -1)
 
-        x = custom_head(custom_head_in_channels, self.out_channels)(x)
+        x = self.custom_head(x)
 
         return x
 
